fc7774edc14df8eb10de5081746a1d3ff488c360
Eduardo <eduardodovale197@gmail.com>
2025-06-04 18:48:13 -0300

    first commit

---
+++ passageiro.cs
@@ -0,0 +1,12 @@
+using System.Security.Cryptography.X509Certificates;
+
+public class Passageiro(
+    //public int id = {get;}
+    //public datetime horaChegada {get;}
+    //public datetime horaEmbarque {get;}
+
+    public
+);
+
+public 
+
---
+++ var.cs
@@ -0,0 +1,9 @@
+public class Vars
+{
+    public int numeroDeCarrinhos { get; set; } = 
+    public int numeroDePassageiros { get; set; } = 
+    public int capacidadeCarrinho { get; set; } = 
+    public DateTime tempoDePasseio { get; set; } = 
+    public DateTime tempoDeEmbarqueDesembarque { get; set; } =
+    public DateTime intervaloPassageiros { get; set; } = 
+}
